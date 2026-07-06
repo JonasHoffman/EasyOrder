f73061df9c5de707fe10b3585b3a35a1265efd64
Jonas de Souza Hoffman <jonas-souza21@hotmail.com>
2026-07-05 23:26:26 -0300

    ajustando urls corretamente

--- EasyOrder/urls.py
+++ EasyOrder/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('lojas/', include('Lojas.urls')),
     path('cardapio/', include('Cardapio.urls')),
-    
+    path("cardapio/categorias/",include(("Cardapio.urls.urls_categorias", "categorias"), namespace="categorias"),),
+    path("cardapio/produto/",include(("Cardapio.urls.urls_produtos", "produtos"), namespace="produtos"),),
+    path("cardapio/grupodesabores/",include(("Cardapio.urls.urls_grupodesabores", "grupodesabores"), namespace="grupodesabores"),),
 ]
 
 if settings.DEBUG:
